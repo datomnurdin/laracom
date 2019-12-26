0cea43c9d1f115f449644e2d2540e24f4a80b118
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2019-12-26 23:40:05 +0800

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,13 +7,33 @@ use Illuminate\Http\Request;
 class CartController extends Controller
 {
     public function index(){
-        //dd(session()->get('success'));
         return view('cart.index');
     }
 
     public function store(Request $request){
+
+        $duplicates = \Cart::search(function($cartItem, $rowId) use ($request){
+            return $cartItem->id === $request->id;
+        });
+
+        if($duplicates->isNotEmpty()){
+            return redirect('/cart')->with('success', 'Item is already in your cart!');
+        }
+
         \Cart::add($request->id, $request->name, 1, $request->price)->associate('App\Product');
         session()->flash('success', 'Item was added to your cart!');
         return redirect('/cart');
+    }
+
+    public function destroy($id){
+        \Cart::remove($id);
+        return back()->with('success', 'Item has been removed!');
+    }
+
+    public function saveForLater($id){
+        $item = \Cart::get($id);
+        \Cart::remove($id);
+        \Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Product');
+        return redirect('/cart')->with('success', 'Item has been Saved For Later');
     }
 }
