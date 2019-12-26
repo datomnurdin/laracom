d1ca2f0b53a5f170cc1d97940244c9c454ef0541
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2019-12-26 09:09:14 +0800

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/products', 'ProductController@index');
+Route::get('/product/{id}', 'ProductController@show');
+
+Route::get('/cart', 'CartController@index');
+Route::post('/cart', 'CartController@store');
