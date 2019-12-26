bae95e84882df9be17a6f1c1a712305b32b2cf6c
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2019-12-26 23:41:05 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ Route::get('/product/{id}', 'ProductController@show');
 
 Route::get('/cart', 'CartController@index');
 Route::post('/cart', 'CartController@store');
+Route::delete('/cart/{id}', 'CartController@destroy');
+Route::post('/cart/saveForLater/{id}', 'CartController@saveForLater');
+
+Route::delete('/saveForLater/{id}', 'SaveForLaterController@destroy');
+Route::post('/saveForLater/moveToCart/{id}', 'SaveForLaterController@moveToCart');
+
+Route::get('/checkout', 'CheckoutController@checkout');
+
+Route::get('/cart/empty', function(){
+    Cart::destroy();
+});
